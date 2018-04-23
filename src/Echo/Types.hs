75408774d1cfd071676aee380e8deae33d68e1a5
sebashack <sebastian0092@gmail.com>
2018-04-22 23:10:56 -0500

    Making binary instances

--- src/Echo/Types.hs
+++ src/Echo/Types.hs
@@ -1,5 +1,7 @@
 module Echo.Types where
 
+import Data.Binary
+
 
 data Operator = Sum
               | Mult
@@ -8,3 +10,31 @@ data Operator = Sum
 data Exp = IntE Int
          | OpE Operator Exp Exp
          deriving Show
+
+instance Binary Operator where
+  put Sum = put (0 :: Word16)
+  put Mult = put (1 :: Word16)
+  get = do
+    t <- get :: Get Word16
+    case t of
+      0 -> return Sum
+      1 -> return Mult
+
+instance Binary Exp where
+  put (IntE i) = do
+    put (0 :: Word8)
+    put i
+  put (OpE op e1 e2) = do
+    put (1 :: Word8)
+    put op
+    put e1
+    put e2
+  get = do
+    t <- get :: Get Word8
+    case t of
+      0 -> get >>= \i -> return (IntE i)
+      1 -> do
+        op <- get :: Get Operator
+        e1 <- get
+        e2 <- get
+        return (OpE op e1 e2)
